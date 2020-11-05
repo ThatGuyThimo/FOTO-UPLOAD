2c81f22758e9da46cf90dc2f0c0b9e0ecc54ae06
Thimo <thimodehaan@gmail.com>
2020-11-05 23:08:41 +0100

    working generate

---
+++ admin/admin dashboard/from.generate.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>generate form</title>
+    </head>
+    <body>
+    <div class="div-Generate">
+
+<form method="get" action="../generate/generate.php">
+
+<table border="0">
+    
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="eventname"></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="submit" value="submit"></td>
+        </tr>
+
+    </table>
+</form>
+</div>
+    </body>
+</html>
--- admin/generate/generate.php
+++ admin/generate/generate.php
@@ -1,16 +1,21 @@
 <?php
 $eventname = $_GET["eventname"];
-$eventindex = "event$eventname";
-$indextext = "<!DOCTYPE html>
-<html lang="eng">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="style.css">
-        <script src="script.js"></script>
-        <title>test site</title>
-    </head>
-    <body>
-        <h1>test site om te kijken of generate.php werkt $eventname</h1>
-    </body>
-</html>"
-$create_file = fopen("$eventindex.php", "W")
+$eventindex = "index.$eventname";
+$eventstyle = "style.$eventname";
+
+$indextext = file_get_contents("test.html");
+
+$styletext = file_get_contents("style.css");
+
+mkdir("../../login/events", $eventname)
+
+$create_file = fopen("$eventindex.php", "w");
+fwrite($create_file, $indextext);
+fclose($create_file);
+
+$create_style = fopen("$eventstyle.css", "w");
+fwrite($create_style, $styletext);
+fclose($create_style);
+
+header("location:$eventindex.php");
+?>

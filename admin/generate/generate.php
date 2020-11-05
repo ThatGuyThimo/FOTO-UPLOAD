285cb3cdbc2cdd4f6f396638c0e491bf4b524e1a
Thimo <thimodehaan@gmail.com>
2020-11-06 00:35:48 +0100

    added comments to generate.php

--- admin/generate/generate.php
+++ admin/generate/generate.php
@@ -1,4 +1,5 @@
 <?php
+// check if usre comes from form.generate.php
 if (isset($_GET['submit'])) {
 
     $eventname = $_GET["eventname"];
@@ -6,23 +7,28 @@ if (isset($_GET['submit'])) {
     $eventstyle = "style.$eventname";
     $path = "../../login/events/$eventname/";
 
+    // grab the content of the files for the event
     $indextext = file_get_contents("index.html");
-
     $styletext = file_get_contents("style.css");
 
+    // make the directory for the new event
     mkdir("$path");
 
+    // create index.html in the specified path
     $create_file = fopen($path, "$eventindex.php", "w");
     fwrite($create_file, $indextext);
     fclose($create_file);
 
+    // create style.css in the specified path
     $create_style = fopen($path, "$eventstyle.css", "w");
     fwrite($create_style, $styletext);
     fclose($create_style);
 
+    // send the user to the new page
     // header("location:$path$eventindex.php");
 
 } else {
+    // if the user does not come from form.generate.php the send him an error
     echo "<p>U komt niet van de generate form</p>";
 }
 ?>

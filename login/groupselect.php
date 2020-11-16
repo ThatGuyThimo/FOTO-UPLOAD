d598aa5d4e4908a54d5aa80e66a0b640262f188d
Jelle <jelle.wijma@gmail.com>
2020-11-16 09:41:39 +0100

    Auto stash before merge of "master" and "origin/master"

--- login/groupselect.php
+++ login/groupselect.php
@@ -13,18 +13,18 @@ require('../includes/config.inc.php');
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
-  <?php
-
-  // maak een query voor de database
-  $userID = $_SESSION['userID'];
-  $query = "SELECT Groupname 
-              FROM group 
+<?php
+    
+    // maak een query voor de database
+    $userID = $_SESSION['userID'];
+    $query = "SELECT Groupname 
+              FROM groups
               WHERE userID = (SELECT userID
                               FROM users
                               WHERE userID = $userID)";
@@ -32,20 +32,18 @@ require('../includes/config.inc.php');
   //voer de query uit
   $result = mysqli_query($mysqli, $query);
 
-  var_dump($result);
+    // loop door alle rijen dat heen
+    while ($row = mysqli_fetch_array($result)) {
+        // start een tabelrij
+        echo "<a href=" . $row['Groupname'] . ">" . $row['Groupname'] . "</a>";
 
-  // loop door alle rijen dat heen
-  while ($row = mysqli_fetch_array($result)) {
-    // start een tabelrij
-    echo "<a herf=" . $row['Groupname'] . ">" . $row['Groupname'] . "</a>";
+        //sluit de tabelrij
+        echo "</tr>";
+    }
 
-    //sluit de tabelrij
-    echo "</tr>";
-  }
-
-  // sluit de tabel
-  echo "</table>";
-  ?>
+    // sluit de tabel
+    echo "</table>"
+    ?>
 </body>
 
 </html>

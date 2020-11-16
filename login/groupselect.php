8fd5bf64ae6d28b1e1055cc74a4150b286710a7b
Jelle <jelle.wijma@gmail.com>
2020-11-16 11:23:10 +0100

    groupselect - group create

--- login/groupselect.php
+++ login/groupselect.php
@@ -13,17 +13,17 @@ require('../includes/config.inc.php');
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-<?php
-    
-    // maak een query voor de database
-    $userID = $_SESSION['userID'];
-    $query = "SELECT Groupname 
+  <?php
+
+  // maak een query voor de database
+  $userID = $_SESSION['userID'];
+  $query = "SELECT Groupname 
               FROM groups
               WHERE userID = (SELECT userID
                               FROM users
@@ -32,18 +32,41 @@ require('../includes/config.inc.php');
   //voer de query uit
   $result = mysqli_query($mysqli, $query);
 
-    // loop door alle rijen dat heen
-    while ($row = mysqli_fetch_array($result)) {
-        // start een tabelrij
-        echo "<a href=" . $row['Groupname'] . ">" . $row['Groupname'] . "</a>";
+  // loop door alle rijen dat heen
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<a href=" . $row['Groupname'] . ">" . $row['Groupname'] . "</a>";
+  }
+  ?>
+  <form action="" method="post">
+    <input type="text" placeholder="Groupname">
+    <div class="list">
+      <?php
+      // maak een query voor de database
+      $query = "SELECT userID, Username FROM user";
 
-        //sluit de tabelrij
-        echo "</tr>";
+      //voer de query uit
+      $result = mysqli_query($mysqli, $query);
+
+      // loop door alle rijen dat heen
+      while ($row = mysqli_fetch_array($result)) {
+        echo "<button>" . $row['Username'] . "<input type='checkbox' name='checkbox" . $row['userID'] . "' id='" . $row['userID'] . "' value='true' ></button>";
+      }
+      ?>
+    </div>
+    <input type="submit" value="submit">
+  </form>
+  <?php
+  if (isset($_POST['submit'])) {
+    echo $_POST['checkbox'];
+    if ($_POST['checkbox'] == 'true') {
+      echo "true";
     }
+    // $Groupname = $_POST['Groupname'];
+    // $groupQuery = "INSERT INTO groups VALUES ($userID,$Groupname,NULL)";
 
-    // sluit de tabel
-    echo "</table>"
-    ?>
+    // $execute = mysqli_query($mysqli, $groupQuery);
+  }
+  ?>
 </body>
 
 </html>

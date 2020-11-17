15131236a4ef0d76fda3995f79ee943c00da7689
Jelle <jelle.wijma@gmail.com>
2020-11-17 15:41:19 +0100

    groupselect - php done

--- login/groupselect.php
+++ login/groupselect.php
@@ -1,51 +1,54 @@
 <?php
 session_start();
-
+// check if the user is loggedin
 if (!isset($_SESSION['Username']) || strlen($_SESSION['Username']) == 0) {
   header("Location:index.php");
   exit;
 }
 
-//lees het config-bestand
+//read the config-file
 require('../includes/config.inc.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Groups</title>
 </head>
+
 <body>
   <?php
 
-  // maak een query voor de database
+  // query every userid in relation with the group
   $userID = $_SESSION['userID'];
   $query = "SELECT Groupname 
               FROM groups
-              WHERE userID IN (SELECT userID
+              WHERE groupID IN (SELECT groupID
                               FROM users
                               WHERE userID = $userID)";
 
-  //voer de query uit
+  //execute the query
   $result = mysqli_query($mysqli, $query);
 
-  // loop door alle rijen dat heen
+  // display every group
   while ($row = mysqli_fetch_array($result)) {
-    echo "<a href=" . $row['Groupname'] . ">" . $row['Groupname'] . "</a><br>";
+    echo "<a href='groups/" . $row['Groupname'] . "'>" . $row['Groupname'] . "</a><br>";
   }
   ?>
   <form action="" method="post">
+    <!-- input the name of the group -->
     <input type="text" name="Groupname" placeholder="Groupname" require>
     <div class="list">
       <?php
-      // maak een query voor de database
+      // query all users
       $query = "SELECT userID, Username FROM user";
 
-      //voer de query uit
+      //execute the query
       $result = mysqli_query($mysqli, $query);
 
-      // loop door alle rijen dat heen
+      // show all the users
       while ($row = mysqli_fetch_array($result)) {
         echo "<button>" . $row['Username'] . "<input type='checkbox' name='member[]' id='" . $row['userID'] . "' value='" . $row['userID'] . "'></button>";
       }
@@ -55,29 +58,67 @@ require('../includes/config.inc.php');
   </form>
   <?php
   if (isset($_POST['submit'])) {
-    // make table groups
+
+    // put the groupname inside a variable 
     $Groupname = $_POST['Groupname'];
 
+    // insert the group name and id and who made the group
     $groupQuery = "INSERT INTO groups VALUES ($userID,'$Groupname',NULL)";
 
     $execute = mysqli_query($mysqli, $groupQuery);
-    // get the group id
+
+    // get the group id from the database
     $getgroupID = "SELECT groupID FROM groups WHERE Groupname = '$Groupname'";
 
     $getID = mysqli_query($mysqli, $getgroupID);
 
+    // put the group number inside a variable
     $groupID = mysqli_fetch_row($getID);
     $GID = $groupID[0];
-    // $groupID[0]
-    // insert user into users with user id and group id
 
+    // insert everu user into users with user id and group id
     $array = $_POST['member'];
     foreach ($array as $member => $value) {
       $createUsers = "INSERT INTO users VALUE($value, $GID)";
       $done = mysqli_query($mysqli, $createUsers);
     }
-    //require "//generate/generate.php";
-    require "/xampp/htdocs/FOTO-UPLOAD/login/generate/generate.php";
+    // put the groupname inside a variable
+    $groupname = $_POST["Groupname"];
+
+    // set the name for the group index page
+    $groupindex = "index";
+
+    // set the right path for the group
+    $path = "groups/$groupname/";
+
+    // grab the content of the files for the group
+    $indextext = file_get_contents("generate/presets/group.homepage.php");
+    $eventtext = file_get_contents("generate/presets/form.event.generate.php");
+    $generate_event_text = file_get_contents("generate/presets/event.generate.php");
+
+    // create the directory for the new group
+    mkdir("$path");
+
+    // create index.html in the specified path
+    $create_file = fopen($path . "$groupindex.php", "w");
+    fwrite($create_file, $indextext);
+    fclose($create_file);
+
+    // create form.event.generate.php
+    $create_file = fopen($path . "form.event.generate.php", "w");
+    fwrite($create_file, $eventtext);
+    fclose($create_file);
+
+    // create the event generater
+    $create_file = fopen($path . "event.generate.php", "w");
+    fwrite($create_file, $generate_event_text);
+    fclose($create_file);
+
+    // create the event directory
+    mkdir("$path/events");
+
+    // send the user to the new page
+    header("location:groupselect.php");
   }
   ?>
 </body>

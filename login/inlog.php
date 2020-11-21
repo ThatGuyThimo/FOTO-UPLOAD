596fb718cfd39eb1d3c601e42fb57367b08d962c
Sander <Sandervanast1@gmail.com>
2020-11-21 11:33:53 +0100

    recaptcha dark/light theme feature

--- login/inlog.php
+++ login/inlog.php
@@ -3,10 +3,21 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
     <meta charset="utf-8">
     <title>Foto-album-Login</title>
     <link rel="stylesheet" href="../style/output/style.css">
+    <script type="text/javascript">
+        const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+
+        var onloadCallback = function() {
+            grecaptcha.render("check", {
+                sitekey: "6LcEJuIZAAAAAA6FGqXZ7QTm3MkvgsuDLjBtSXJP",
+                theme: userPrefersDark ? 'dark' : 'light',
+            });
+        }
+    </script>
 </head>
 
 <body>
@@ -14,27 +25,29 @@
         <div class="header">Inloggen</div>
     </div>
     <main>
-    <div class="inlogdiv">
-        <form method="POST" action="inlog.verwerk.php" enctype="multipart/form-data">
-            <div class="Form">
-                <div class="formItem">
-                    <label for="Username">Gebruikersnaam</label>
-                    <input type="text" name="Username">
+        <div class="inlogdiv">
+            <form method="POST" action="inlog.verwerk.php" enctype="multipart/form-data">
+                <div class="Form">
+                    <div class="formItem">
+                        <label for="Username">Gebruikersnaam</label>
+                        <input type="text" name="Username">
+                    </div>
+                    <div class="formItem">
+                        <label for="Username">Wachtwoord</label>
+                        <input type="password" name="Password" id="myInput">
+                    </div>
+                    <div class="formItem">
+                        <div class="g-recaptcha" id="check"></div>
+                    </div>
                 </div>
-                <div class="formItem">
-                    <label for="Username">Wachtwoord</label>
-                    <input type="password" name="Password" id="myInput">
+                <div class="formItem submitBtn">
+                    <input class="Btn_login" type="submit" name="Inlog" value="inloggen">
                 </div>
-                <div class="formItem">
-                    <div class="g-recaptcha" data-sitekey="6Lcgd-IZAAAAALDXzqOGaBXWmJHCuA71_g3LiQ2T"></div>
-                </div>
-            </div>
-            <div class="formItem submitBtn">
-                <input class="Btn_login" type="submit" name="Inlog" value="inloggen">
-            </div>
-        </form>
-    </div>
+            </form>
+        </div>
     </main>
+
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer></script>
 </body>
 
 

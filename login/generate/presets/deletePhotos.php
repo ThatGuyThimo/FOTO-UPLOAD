1fcaafd4d0ed5e5c179bae323dc241fc7ef76853
Jelle <jelle.wijma@gmail.com>
2020-12-07 12:21:47 +0100

    delete done

--- login/generate/presets/deletePhotos.php
+++ login/generate/presets/deletePhotos.php
@@ -29,64 +29,62 @@ $userID = $_SESSION["userID"];
     <div class="header"><?php echo $eventname . " - "; ?></div>
     <a href="index.php">Back</a>
   </div>
-  <div class="photo's">
-    <!-- show all the pictures inside the folder -->
-    <?php
-    // query every userid in relation with the group
-    $query = "SELECT link
+  <form method="post">
+    <div class="photo's">
+      <!-- show all the pictures inside the folder -->
+      <?php
+      // query every userid in relation with the group
+      $query = "SELECT link, ID
                   FROM images
                   INNER JOIN event ON event.eventID = images.eventID
                   WHERE Eventname = '$eventname' AND userID = $userID";
-    //execute the query
-    $result = mysqli_query($mysqli, $query);
+      //execute the query
+      $result = mysqli_query($mysqli, $query);
 
-    // loop door alle rijen dat heen
-    // make an if loop to check if there is something inside the query
-    if (!$result) {
-      echo "<h2>There are on event yet...<h2>";
-    } else {
-      while ($row = mysqli_fetch_array($result)) {
-    ?>
-        <input type="checkbox" name="" id=""><img src="<?php echo "../$eventname/photos/" . $row['link']; ?>" alt="" srcset="" style="width: 50px;"></input>
-        
-    <?php
+      // loop door alle rijen dat heen
+      // make an if loop to check if there is something inside the query
+      if (!$result) {
+        echo "<h2>There are on event yet...<h2>";
+      } else {
+        while ($row = mysqli_fetch_array($result)) {
+      ?>
+          <input type="checkbox" name="<?php echo 'photo[]' ?>" id="<?php echo $row['ID'] ?>" value="<?php echo $row['ID'] ?>">
+          <img src="<?php echo "../$eventname/photos/" . $row['link']; ?>" style="width: 100px;" for="<?php echo $row['ID'] ?>"></input>
+
+      <?php
+        }
       }
-    }
-    ?>
-  </div>
-  <form method="post" enctype="multipart/form-data">
-    <input type="file" name="photo" id="photo">
-    <input type="submit" name="submit" id="submit">
+      ?>
+    </div>
+
+    <input type="submit" name="submit" value="delete" id="submit">
   </form>
 </body>
 
 </html>
 <?php
 if (isset($_POST['submit'])) {
-  if (isset($_FILES['photo']) && $_FILES['photo']['error'] == 0) {
-    $map = __DIR__ . "/photos/";
 
-    $file = $_FILES['photo']['name'];
+  // delete the photo out of the database
+  $array = $_POST['photo'];
+  foreach ($array as $photo => $value) {
+    // get the name of the photo
+    $getname = "SELECT link
+              FROM images
+              WHERE ID = $value";
+    $sendquery = mysqli_query($mysqli, $getname);
+    // get the 
+    $rawname = mysqli_fetch_array($sendquery);
 
-    move_uploaded_file($_FILES['photo']['tmp_name'], $map . $file);
+    $filename = $rawname['link'];
 
-    $searchID = "SELECT eventID
-              FROM event
-              WHERE Eventname = '$eventname'";
-    //execute the query
-    $newsearchID = mysqli_query($mysqli, $searchID);
+    unlink("photos/" . $filename);
 
-    echo $eventname;
-
-    $raweventID = mysqli_fetch_array($newsearchID);
-
-    $eventID = $raweventID['eventID']; 
-
-    $upload = "INSERT INTO images VALUES (NULL, '$file', $userID, $eventID)";
-
-    $execute = mysqli_query($mysqli, $upload);
-
-    header("Location:index.php");
+    $deletephoto = "DELETE FROM images WHERE ID = $value";
+    $done = mysqli_query($mysqli, $deletephoto);
   }
+
+  header("Location:index.php");
+
 }
 ?>

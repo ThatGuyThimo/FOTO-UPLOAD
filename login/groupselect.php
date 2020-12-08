4182a8b5d82df48be951722c19ff795484fca88b
Jelle <jelle.wijma@gmail.com>
2020-12-08 16:32:45 +0100

    check if groupname exits

--- login/groupselect.php
+++ login/groupselect.php
@@ -45,13 +45,13 @@ require('../includes/config.inc.php');
         // loop door alle rijen dat heen
         while ($row = mysqli_fetch_array($result)) {
         ?>
-          <a href="groups/<?php echo $row['Groupname'] ?>">
-            <div class="groupItem">
-              <div class="group">
-                <?php echo $row['Groupname'] ?>
-              </div>
+        <a href="groups/<?php echo $row['Groupname'] ?>">
+          <div class="groupItem">
+            <div class="group">
+              <?php echo $row['Groupname'] ?>
             </div>
-          </a>
+          </div>
+        </a>
         <?php
         }
         ?>
@@ -73,12 +73,13 @@ require('../includes/config.inc.php');
           // show all the users
           while ($row = mysqli_fetch_array($result)) {
           ?>
-            <div class="userItem">
-              <input class="input" type="checkbox" name="<?php echo 'member[]' ?>" value="<?php echo $row['userID'] ?>" id="<?php echo $row['userID'] ?>">
-              <label for="<?php echo $row['userID'] ?>">
-                <?php echo $row['Username'] ?>
-              </label>
-            </div>
+          <div class="userItem">
+            <input class="input" type="checkbox" name="<?php echo 'member[]' ?>" value="<?php echo $row['userID'] ?>"
+              id="<?php echo $row['userID'] ?>">
+            <label for="<?php echo $row['userID'] ?>">
+              <?php echo $row['Username'] ?>
+            </label>
+          </div>
           <?php
           }
           ?>
@@ -95,88 +96,100 @@ require('../includes/config.inc.php');
     // put the groupname inside a variable 
     $Groupname = $_POST['Groupname'];
 
-    // insert the group name and id and who made the group
-    $groupQuery = "INSERT INTO groups VALUES ($userID,'$Groupname',NULL)";
+    $checkgroupname = "SELECT Groupname FROM groups WHERE Groupname = '$Groupname'";
 
-    $execute = mysqli_query($mysqli, $groupQuery);
+    $testquery = mysqli_query($mysqli, $checkgroupname);
 
-    // get the group id from the database
-    $getgroupID = "SELECT groupID FROM groups WHERE Groupname = '$Groupname'";
+    // check if the group name already exists
+    if (mysqli_num_rows($testquery) == 0) {
+        // insert the group name and id and who made the group
+      $groupQuery = "INSERT INTO groups VALUES ($userID,'$Groupname',NULL)";
 
-    $getID = mysqli_query($mysqli, $getgroupID);
+      $execute = mysqli_query($mysqli, $groupQuery);
 
-    // put the group number inside a variable
-    $groupID = mysqli_fetch_row($getID);
-    $GID = $groupID[0];
+      // get the group id from the database
+      $getgroupID = "SELECT groupID FROM groups WHERE Groupname = '$Groupname'";
 
-    // insert everu user into users with user id and group id
-    $array = $_POST['member'];
-    foreach ($array as $member => $value) {
-      $createUsers = "INSERT INTO users VALUE($value, $GID)";
-      $done = mysqli_query($mysqli, $createUsers);
+      $getID = mysqli_query($mysqli, $getgroupID);
+
+      // put the group number inside a variable
+      $groupID = mysqli_fetch_row($getID);
+      $GID = $groupID[0];
+
+      // insert everu user into users with user id and group id
+      $array = $_POST['member'];
+      foreach ($array as $member => $value) {
+        $createUsers = "INSERT INTO users VALUE($value, $GID)";
+        $done = mysqli_query($mysqli, $createUsers);
+      }
+      // put the groupname inside a variable
+      $groupname = $_POST["Groupname"];
+
+      // set the name for the group index page
+      $groupindex = "index";
+
+      // set the right path for the group
+      $path = "groups/$groupname/";
+
+      // grab the content of the files for the group
+      $indextext = file_get_contents("generate/presets/group.homepage.php");
+      $eventtext = file_get_contents("generate/presets/form.event.generate.php");
+      $generate_event_text = file_get_contents("generate/presets/event.generate.php");
+      $deletetext = file_get_contents("generate/presets/delete.php");
+      $ajaxtext = file_get_contents("generate/presets/ajax.js");
+      $delete_form_html = file_get_contents("generate/presets/delete_form.html");
+
+      // create the directory for the new group
+      mkdir("$path");
+
+      // create index.html in the specified path
+      $create_file = fopen($path . "$groupindex.php", "w");
+      fwrite($create_file, $indextext);
+      fclose($create_file);
+
+      // create form.event.generate.php
+      $create_file = fopen($path . "form.event.generate.php", "w");
+      fwrite($create_file, $eventtext);
+      fclose($create_file);
+
+      // create the event generater
+      $create_file = fopen($path . "event.generate.php", "w");
+      fwrite($create_file, $generate_event_text);
+      fclose($create_file);
+
+      // create the delete page
+      $create_file = fopen($path . "delete.php", "w");
+      fwrite($create_file, $deletetext);
+      fclose($create_file);
+
+      // create the delete page
+      $create_file = fopen($path . "delete.php", "w");
+      fwrite($create_file, $deletetext);
+      fclose($create_file);
+
+      // create the ajax page
+      $create_file = fopen($path . "ajax.js", "w");
+      fwrite($create_file, $ajaxtext);
+      fclose($create_file);
+
+      // create the deleteform page
+      $create_file = fopen($path . "delete_form.html", "w");
+      fwrite($create_file, $delete_form_html);
+      fclose($create_file);
+
+      // create the event directory
+      mkdir("$path/events");
+
+      // send the user to the new page
+      //header("location:groupselect.php");
+      // sort of fix (the bug was only present on Jelle's device)
+      echo "<script>window.location.href='groupselect.php'</script>";
+    } else {
+      echo "<script>alert('Groupname already exists');</script>";
+      echo "<script>window.location.href='groupselect.php'</script>";
     }
-    // put the groupname inside a variable
-    $groupname = $_POST["Groupname"];
 
-    // set the name for the group index page
-    $groupindex = "index";
-
-    // set the right path for the group
-    $path = "groups/$groupname/";
-
-    // grab the content of the files for the group
-    $indextext = file_get_contents("generate/presets/group.homepage.php");
-    $eventtext = file_get_contents("generate/presets/form.event.generate.php");
-    $generate_event_text = file_get_contents("generate/presets/event.generate.php");
-    $deletetext = file_get_contents("generate/presets/delete.php");
-    $ajaxtext = file_get_contents("generate/presets/ajax.js");
-    $delete_form_html = file_get_contents("generate/presets/delete_form.html");
-
-    // create the directory for the new group
-    mkdir("$path");
-
-    // create index.html in the specified path
-    $create_file = fopen($path . "$groupindex.php", "w");
-    fwrite($create_file, $indextext);
-    fclose($create_file);
-
-    // create form.event.generate.php
-    $create_file = fopen($path . "form.event.generate.php", "w");
-    fwrite($create_file, $eventtext);
-    fclose($create_file);
-
-    // create the event generater
-    $create_file = fopen($path . "event.generate.php", "w");
-    fwrite($create_file, $generate_event_text);
-    fclose($create_file);
-
-    // create the delete page
-    $create_file = fopen($path . "delete.php", "w");
-    fwrite($create_file, $deletetext);
-    fclose($create_file);
-
-    // create the delete page
-    $create_file = fopen($path . "delete.php", "w");
-    fwrite($create_file, $deletetext);
-    fclose($create_file);
-
-    // create the ajax page
-    $create_file = fopen($path . "ajax.js", "w");
-    fwrite($create_file, $ajaxtext);
-    fclose($create_file);
-
-    // create the deleteform page
-    $create_file = fopen($path . "delete_form.html", "w");
-    fwrite($create_file, $delete_form_html);
-    fclose($create_file);
-
-    // create the event directory
-    mkdir("$path/events");
-
-    // send the user to the new page
-    //header("location:groupselect.php");
-    // sort of fix (the bug was only present on Jelle's device)
-    echo "<script>window.location.href='groupselect.php'</script>";
+    
   }
   ?>
 </body>
